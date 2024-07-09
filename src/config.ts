23ae0cec6dc820b72a17011527fa0763c55c698e
stackgik <stackgik@gmail.com>
2024-07-09 17:03:39 +0100

    stop tracking the config file

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-  tmdbToken: import.meta.env.VITE_APP_TMDB_TOKEN,
-  supabaseKey: import.meta.env.VITE_GIKEO_SUPABASE_KEY,
-  supabaseUrl: import.meta.env.VITE_GIKEO_SUPABASE_URL,
-};
