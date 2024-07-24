7fa6b16922bcb913ec0a0f62ec3f2fc2823544ff
stackgik <stackgik@gmail.com>
2024-07-24 16:46:12 +0100

    solve the bug in my searchMedia by using handling setParams and setPage as side effect

--- src/features/search/useSearchQuery.ts
+++ src/features/search/useSearchQuery.ts
@@ -2,24 +2,44 @@ import { useQuery } from "@tanstack/react-query";
 import { getQueryResults } from "../../services/apiSearch";
 import { type SimilarMoviesResponse } from "../../services/apiMovies";
 import { useSearchParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-// prettier-ignore
 export const useSearchQuery = () => {
-    const [searchParams, setSearchParams] = useSearchParams()
-    const query = searchParams.get('query') as string
-    const [page, setPage] = useState(1)
-    searchParams.set('page', String(page));
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("query") as string;
+  const [page, setPage] = useState(1);
+  const [dataLen, setDataLen] = useState<number | null>(null);
 
-
-  const {data: queryResults, isLoading: isQueryResultsLoading, error: queryResultsError} = useQuery<SimilarMoviesResponse, Error>({
+  const {
+    data: queryResults,
+    isLoading: isQueryResultsLoading,
+    error: queryResultsError,
+  } = useQuery<SimilarMoviesResponse, Error>({
     queryKey: ["search", query, page],
     queryFn: () => getQueryResults(query, page),
     retry: false,
     enabled: !!query,
   });
 
-  if(queryResults?.results.length !== 0) setSearchParams(searchParams); 
+  useEffect(() => {
+    if (queryResults) {
+      setDataLen(queryResults.results.length);
+    }
+  }, [queryResults]);
 
-  return { queryResults, isQueryResultsLoading, queryResultsError, page, setPage, query };
+  useEffect(() => {
+    if (dataLen !== null && dataLen !== 0) {
+      searchParams.set("page", String(page));
+      setSearchParams(searchParams);
+    }
+  }, [page, searchParams, setSearchParams, dataLen]);
+
+  return {
+    queryResults,
+    isQueryResultsLoading,
+    queryResultsError,
+    page,
+    setPage,
+    query,
+  };
 };
