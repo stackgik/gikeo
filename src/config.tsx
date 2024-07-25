aee3004d1d8c654e405da03fc4b373000147da34
stackgik <stackgik@gmail.com>
2024-07-25 16:16:10 +0100

    change config file extension from ts to tsx

---
+++ src/config.tsx
@@ -0,0 +1,5 @@
+export const config = {
+  tmdbToken: import.meta.env.VITE_APP_TMDB_TOKEN,
+  supabaseKey: import.meta.env.VITE_GIKEO_SUPABASE_KEY,
+  supabaseUrl: import.meta.env.VITE_GIKEO_SUPABASE_URL,
+};
