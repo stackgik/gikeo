0af874dc9078ea1ae138e394f0eb9eda2bc58b74
stackgik <uc@ucs-MacBook-Air.local>
2025-09-18 14:12:42 +0100

    refactor: modify the SimilarMedia component to use simple scroll display

--- src/ui/SimilarMedia.tsx
+++ src/ui/SimilarMedia.tsx
@@ -1,12 +1,3 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-
 import Tag from "./Tag";
 import useMovieDetails from "../features/movies/useMovieDetails";
 import MediaCard from "./MediaCard";
@@ -35,53 +26,27 @@ const SimilarMedia = () => {
   if (!refinedSimilarMovies?.length) return "";
 
   return (
-    <section className="mx-auto w-custom-min-width">
+    <section className="mx-auto mt-24 grid w-custom-min-width gap-16">
       <Tag>{`Similar ${mediaType}S`}</Tag>
+
+      <div className="scrollbar-hide flex gap-4 overflow-x-scroll">
+        {isMovieDetailsLoading
+          ? ["a", "b", "c", "d", "e", "f"].map((_, index) => (
+              <SkeletonCard key={index} />
+            ))
+          : refinedSimilarMovies?.map((similarMovie) => (
+              <MediaCard
+                mediaData={similarMovie}
+                mediaType={"movie"}
+                key={similarMovie.id}
+              />
+            ))}
+      </div>
       {!refinedSimilarMovies?.length ? (
         <p className="mt-6 text-[1.5rem] font-medium dark:text-dark-grey-500">
           {`Oh oh! There is no similar ${mediaType === "tv" ? "TV shows" : mediaType} to display`}
         </p>
-      ) : (
-        <Swiper
-          loop={false}
-          grabCursor={true}
-          spaceBetween={20}
-          slidesPerView={6.6}
-          breakpoints={{
-            0: {
-              slidesPerView: 1.6,
-            },
-            401: {
-              slidesPerView: 2.3,
-            },
-            701: {
-              slidesPerView: 4.3,
-            },
-            1025: {
-              slidesPerView: 4.2,
-            },
-            1201: {
-              slidesPerView: 5.3,
-            },
-            1441: {
-              slidesPerView: 6.6,
-            },
-          }}
-          className="mb-8 mt-16 w-full"
-        >
-          {isMovieDetailsLoading
-            ? ["a", "b", "c", "d", "e", "f"].map((_, index) => (
-                <SwiperSlide key={index}>
-                  <SkeletonCard />
-                </SwiperSlide>
-              ))
-            : refinedSimilarMovies?.map((similarMovie) => (
-                <SwiperSlide key={similarMovie.id}>
-                  <MediaCard mediaData={similarMovie} mediaType={"movie"} />
-                </SwiperSlide>
-              ))}
-        </Swiper>
-      )}
+      ) : null}
     </section>
   );
 };
